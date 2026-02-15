ce9e2c47fb26a393a3d569bd03e4f56fb336aa85
El tío judai <alexnarcista@gmail.com>
2026-02-15 11:10:52 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,10 @@
 export const config = {
     sessions: 'EmpireSession',
-    // Esta regex permite detectar el símbolo al inicio
+    // Esta regex detecta el símbolo al inicio y permite flexibilidad
     prefix: /^[#!./-]/, 
     botName: 'Empire Bot',
-    owner: '50360438371'
+    // Reemplaza con tu número real (con código de país y sin el +)
+    owner: '50360438371', 
+    // Texto que aparecerá al final de algunos mensajes
+    marca: '🛡️ *E M P I R E  B O T*' 
 };
